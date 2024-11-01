5b80b0fe7e25104d64c21f9a794dc6ce4e7670db
Anastasiya-Vavilova <ntttru@bk.ru>
2024-11-01 21:56:48 +0300

    finalization of tests

--- src/test/java/OrderACardTest.java
+++ src/test/java/OrderACardTest.java
@@ -31,45 +31,80 @@ public class OrderACardTest {
         driver = null;
     }
 
-    @Test
-        // Тест 1 Фамилия и имя
-    void orderACard() throws InterruptedException {
+    @Test // Отправка Ф.И. русскими буквами включая букву ё.
+    void letterE() throws InterruptedException {
         WebElement form = driver.findElement(By.className("form_size_m"));
-        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Вавилова Анастасия");
-        String actual = form.findElement(By.className("input__control")).getAttribute("value");
-        String expected = "Вавилова Анастасия";
+        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Ёжикина Анастасия");
+        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79655844662");
+        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        form.findElement(By.cssSelector("[role=button]")).click();
+        String expected = driver.findElement(By.className("paragraph")).getAttribute("data-test-id");
+        String actual = null;
         Assertions.assertEquals(expected, actual);
-
     }
 
-    @Test
-        // Тест 2 Номер телефона
-    void orderACaryd() throws InterruptedException {
+    @Test // Отправка Ф.И. включающая в себя дефисы.
+    void hyphen() throws InterruptedException {
         WebElement form = driver.findElement(By.className("form_size_m"));
-        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("79998153516");
-        String actual = form.findElement(By.cssSelector("[data-test-id=phone] input")).getAttribute("value");
-        String expected = "79998153516";
+        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Кириллова-Угрюмова Алена");
+        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79655844662");
+        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        form.findElement(By.cssSelector("[role=button]")).click();
+        WebElement form2 = driver.findElement(By.className("App_appContainer__3jRx1"));
+        String actual = form2.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        String expected = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         Assertions.assertEquals(expected, actual);
-
     }
 
+    @Test // Отправка Ф.И. содержащая пробелы.
+    void spase() throws InterruptedException {
+        WebElement form = driver.findElement(By.className("form_size_m"));
+        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Нурбела Огли Алена");
+        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79655844662");
+        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        form.findElement(By.cssSelector("[role=button]")).click();
+        WebElement form2 = driver.findElement(By.className("App_appContainer__3jRx1"));
+        String actual = form2.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        String expected = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        Assertions.assertEquals(expected, actual);
+    }
 
-    @Test // Тест 3 Ошибка Имя и Фамилия указаные неверно.
-    public void latinName() throws InterruptedException {
+    @Test // Ф.И. состоящее из кириллических букв.
+    public void cyrillic() throws InterruptedException {
         WebElement form = driver.findElement(By.className("form_size_m"));
         form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Vavilova Anastasiya");
-        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79999999999");
-        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         form.findElement(By.cssSelector("[role=button]")).click();
         String actual = form.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText();
         String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test //Тест 4 Ошибка Поле обязательно для заполнения"
-    public void NullName() throws InterruptedException {
+    @Test // Ф.И. состоящее из спецсимволов.
+    public void specialCharacters() throws InterruptedException {
         WebElement form = driver.findElement(By.className("form_size_m"));
-        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79999999999");
+        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Вавилова Анастасия.");
+        form.findElement(By.cssSelector("[role=button]")).click();
+        String actual = form.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText();
+        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test // Ф.И. состоящее с буквой - й.
+    public void letterTh() throws InterruptedException {
+        WebElement form = driver.findElement(By.className("form_size_m"));
+        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Лукьянов Йосиф");
+        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79655844662");
+        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        form.findElement(By.cssSelector("[role=button]")).click();
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        String expected = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test // Отправка формы с незаполненным Ф.И..
+    public void nullName() throws InterruptedException {
+        WebElement form = driver.findElement(By.className("form_size_m"));
+        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79655844662");
         form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         form.findElement(By.cssSelector("[role=button]")).click();
         String actual = form.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText();
@@ -77,8 +112,41 @@ public class OrderACardTest {
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test //Тест 5 Ошибка Невалидный номер телефона
-    public void InvalidNumber() throws InterruptedException {
+    @Test // Отправка формы с незаполненным полем телефона.
+    public void nullNumber() throws InterruptedException {
+        WebElement form = driver.findElement(By.className("form_size_m"));
+        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Анастасия");
+        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        form.findElement(By.cssSelector("[role=button]")).click();
+        String actual = form.findElement(By.cssSelector("[data-test-id=phone] .input__sub")).getText();
+        String expected = "Поле обязательно для заполнения";
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test // Отправка заявки с заполненным полем только номер телефона.
+    public void oneNumber() throws InterruptedException {
+        WebElement form = driver.findElement(By.className("form_size_m"));
+        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79655844662");
+        form.findElement(By.cssSelector("[role=button]")).click();
+        String actual = form.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText();
+        String expected = "Поле обязательно для заполнения";
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test // Отправка заявки с номером начинающимся с цифры 8.
+    public void numberEight() throws InterruptedException {
+        WebElement form = driver.findElement(By.className("form_size_m"));
+        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Вавилова Анастасия");
+        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("89655844662");
+        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        form.findElement(By.cssSelector("[role=button]")).click();
+        String actual = form.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText();
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test // Отправка заявки с номером состоящим из менее 11 цифр.
+    public void lessThan11() throws InterruptedException {
         WebElement form = driver.findElement(By.className("form_size_m"));
         form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Вавилова Анастасия");
         form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79997377");
@@ -89,16 +157,41 @@ public class OrderACardTest {
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test  // Тест 6 Успешная отправка формы
+    @Test // Отправка заявки с номером состоящим из более 8 цифр.
+    public void moreThan8() throws InterruptedException {
+        WebElement form = driver.findElement(By.className("form_size_m"));
+        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Вавилова Анастасия");
+        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("896558446622");
+        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        form.findElement(By.cssSelector("[role=button]")).click();
+        String actual = form.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText();
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test // Успешная отправка формы с валидными данными.
     void nextForm() throws InterruptedException {
         WebElement form = driver.findElement(By.className("form_size_m"));
         form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Вавилова Анастасия");
-        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79999999999");
+        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79655844662");
         form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         form.findElement(By.cssSelector("[role=button]")).click();
         WebElement form2 = driver.findElement(By.className("App_appContainer__3jRx1"));
         String actual = form2.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
         String expected = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         Assertions.assertEquals(expected, actual);
+    }
+
+    @Test // Отправка формы с незаполненным чек боксом.
+    void checkbox() throws InterruptedException {
+        WebElement form = driver.findElement(By.className("form_size_m"));
+        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Вавилова Анастасия");
+        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79655844662");
+        form.findElement(By.cssSelector("[role=button]")).click();
+        String nameClass = "checkbox_checked";
+        Boolean expected = false;
+        String actual = form.findElement(By.cssSelector("label")).getAttribute("class");
+        Boolean result = actual.contains(nameClass);
+        Assertions.assertEquals(expected, result);
     }
 }
